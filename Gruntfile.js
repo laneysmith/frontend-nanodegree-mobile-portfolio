6c01f78ee3852ce9a5a983e120c166ff7532ba2d
Laney Smith <lsmith.unc@gmail.com>
2015-06-26 20:27:37 -0600

    Inlined CSS, etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function(grunt) {
       dynamic: {                         // Another target
         files: [{
           expand: true,                  // Enable dynamic expansion
-          cwd: 'src/',                   // Src matches are relative to this path
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
           dest: 'dist/'                  // Destination path prefix
         }]
